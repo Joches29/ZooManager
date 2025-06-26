9256dafeb2e4211a6023c7ed66e3b0cdc9229197
Leandro Sanchez Rojas <lesanchezro@est.utn.ac.cr>
2025-06-26 13:26:53 -0600

    Lists add
    se agrego el paquete Lists con la interface List agregando el metodo add

---
+++ src/Lists/List.java
@@ -0,0 +1,14 @@
+package Lists;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author llean
+ */
+public interface List <T> {
+    public boolean add(T t);
+}
