62c906675d736718227f01067187f55d016476ae
Joches29 <jochaveshe@est.utn.ac.cr>
2025-08-07 15:08:52 -0600

    Se crea la clase AnimalsHashMapList
    Se agrega la clase que implemente la interface para utilizar un HashMap para añadir, eliminar, buscar y mostrar los animales.

---
+++ src/Animals/AnimailsHashMapList.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Animals;
+
+import Lists.List;
+import java.util.HashMap;
+
+/**
+ *
+ * @author rodol
+ */
+public class AnimailsHashMapList implements List <Animal> {
+    HashMap<String, Animal> map;
+
+    public AnimailsHashMapList() {
+        this.map = new HashMap<>();
+    }
+
+    @Override
+    public boolean add(Animal t) {
+        if (map.containsKey(t.getId())) {
+            return false;
+        }
+        map.put(t.getId(), t);
+        return true;
+    }
+
+    @Override
+    public boolean remove(Animal t) {
+        return map.remove(t.getId(), map);
+    }
+
+    @Override
+    public Animal find(Object id) {
+        String strId = String.valueOf(id);
+        return map.get(strId);
+    }
+
+    @Override
+    public void showAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
