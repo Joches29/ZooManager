37c07df3d3be872e37754f98840acd7b4655445b
Joches29 <jochaveshe@est.utn.ac.cr>
2025-08-15 14:16:47 -0600

    Se crea la interface GUI
    Se agrega una interface con metodo genericos para poder generalizar muchos comportamientos que las clases podrian compartir.

---
+++ src/GUI/Gui.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package GUI;
+
+/**
+ *
+ * @author rodol
+ */
+public interface Gui {
+
+    public void clear();
+
+    public boolean validateRequiere();
+
+    public void save();
+
+    public void update();
+
+    public void delete();
+
+    public void search();
+
+    public void showData();
+
+}
